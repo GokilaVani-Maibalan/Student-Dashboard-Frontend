cdb49f6bd493d55a5676ef484e6afc49f9f685bd
GokilaVani-Maibalan <121846936+GokilaVani-Maibalan@users.noreply.github.com>
2024-04-11 11:21:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 
 import { Navigation } from "./Navigation";
 
-export const url = "http://localhost:3001";
+export const url = "https://deft-valkyrie-f4cfdc.netlify.app/";
 // ("http://localhost:3000");
 
 function App() {
